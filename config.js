a6274ac372861f10874229229501f7f4e4ed572a
Eduardo K. M. Ogawa <37462412+ekmogawa@users.noreply.github.com>
2026-03-22 19:47:21 -0300

    Update GitHub configuration with new token and salt

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // CONFIGURAÇÃO DO GITHUB — Gerar Laudo EDA
 // Token criptografado com AES-256-GCM + PBKDF2.
 // Repositório pode ser público — sem a senha o token é inútil.
-// Gerado em: 22/03/2026, 19:26:42
+// Gerado em: 22/03/2026, 19:47:09
 // ============================================================
 
 const GITHUB_CONFIG = {
@@ -12,7 +12,7 @@ const GITHUB_CONFIG = {
   path:   'dados_eda.js',
 
   // Token criptografado — NÃO edite estes valores manualmente
-  tokenCriptografado: 'bJwWj/BVySW3jbjDv5iaxnDzUBW7fATeq8dDiImtGvKnC4+hC2YGMeIvgHaLPkSS1io8CrtSbLVCy8tE36kO/s7kHMk2/v0J4YV7kl0FYd2JX7cZpTyg9khF3xnsbkJ3fvTTmu0fhd9jGggNKjThWT7qcT/0XfRhi6K7wV1prCZZYCNWWiu+MnYPY46EF0y3dOEA30uMJ2F48jI2UHDoSL2/h6SAYw3TcyarcowKnJMJGdHdALJynm+lu2IQsXWdOfSdiipqNk063+9oXCgvzJDqGYSN6BHIdM93M7t31CkSjIyojWiT+HTeXzb/nQG+kbyKosgvxUQUr6PBct1RaCjLMJbmK47wh95N2WRvpTdWLg3sKJxiwQzbnl5PAa7UhIK0XuKVJZoVuN/ehkLOyFEjFMAJz+hop/SqSK9qiFAceB40L5xUxBdpr9kNTrAWFvaAPkIJk7Bqybddl+uj/tDEU6ipIfVWR2D/hCWu2lGkk8rjYvOu5ylSHZPLnNaZpqk4Z/h6TQCS5i0uIhCRhX8eIXq3H26I8Q9mL+xGtbLgESHVft+aYZTDNAgBYgedAvSx0DugycdVTDzHicEEWh1XxoUbiEFVv7fEg+g32nh5vL8OAygSbTX/V6TWNdCkdqM57b7gEkzhEm3XQYVoVuBVJA4upQEAej7KTqFTH/HOYvwBa0f4D2wfZiGeiCjmeoXDejyBHzJeovGbzALaecMB9kHLLl0pbNmD/PXhKDoFc/XaCULmGrn7m5nNd1MhN4Of+9jwiPwTDzosIlN0B8dZ3kgy3BNPBEyZ3Wuci8zFBkUCpwQOLLgwdxytSWhAYFlJyDSaA48ttOO24Hl7M961FwvDH0HoaFVrK1uGbXzit/H9hfacVJ2j+3Xin6qfUsOY4fof9g==',
-  salt: 'mH/WeGvlaQDyJiF7L6rStQ==',
-  iv:   'yDhrkXz750+vyDVJ',
+  tokenCriptografado: 'aqjnz8ssDjBV5wIfWTWYJMvFO+ndNJnthxX+aS8/1QR+/KT7D1O56MttBbYGxxIX1DpUZfCl2TM=',
+  salt: 'yrIq0Rsc/ayarfirPtmUDg==',
+  iv:   '5T9EtalxCxxwyQVn',
 };
